804d7ff157be352904a473652965a283c1a44d55
snapiz <jonathan.lapiquonne@gmail.com>
2023-08-11 01:00:00 +0200

    fix(publish): not check cargoPublish from plugin config

--- src/publish.ts
+++ src/publish.ts
@@ -2,6 +2,10 @@ import { Context } from "semantic-release";
 import { cargoPublish, getCargoMetadata } from "./cargo";
 
 export default function (config: PluginConfig, context: Context) {
+  if (!config.cargoPublish) {
+    return;
+  }
+
   const data = getCargoMetadata();
 
   for (const cargoPackage of data.packages) {
